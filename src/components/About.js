d2682060f948ba05a377c507366fb97d7fc3f78a
Rochelle Awuor <awuorochelle75@gmail.com>
2025-04-14 23:33:07 +0300

    modifies src/

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
   <div id="about">
    <h2>About Me </h2>
    <p>I am based in Nairobi,Kenya.</p>
-   <img src={ image } alt="I made this" />
+   <img src={image} alt="I made this" />
     </div>
     );
 }
